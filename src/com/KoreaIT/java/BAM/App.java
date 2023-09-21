4be403512c94719afe8c7d59cd64d1320e3ee4da
jominseok0513 <jominseok0513@gmail.com>
2023-09-21 13:56:54 +0900

    게시판 로그인 만들기

---
+++ src/com/KoreaIT/java/BAM/App.java
@@ -0,0 +1,70 @@
+package com.KoreaIT.java.BAM;
+
+import java.util.Scanner;
+
+import com.KoreaIT.java.BAM.controller.ArticleController;
+import com.KoreaIT.java.BAM.controller.Controller;
+import com.KoreaIT.java.BAM.controller.MemberController;
+
+public class App {
+
+	App() {
+
+	}
+
+	public void start() {
+		System.out.println("== 프로그램 시작 ==");
+
+		Scanner sc = new Scanner(System.in);
+
+		MemberController memberController = new MemberController(sc);
+		ArticleController articleController = new ArticleController(sc);
+
+		articleController.makeTestData();
+		memberController.makeTestData_member();
+		Controller controller;
+
+		while (true) {
+
+			System.out.printf("명령어 ) ");
+			String command = sc.nextLine().trim();
+
+			if (command.length() == 0) {
+				System.out.println("너 명령어 입력 안했어");
+				continue;
+			}
+
+			if (command.equals("exit")) {
+				break;
+			}
+
+			String[] commandDiv = command.split(" ");
+
+			String controllerName = commandDiv[0];
+			if (commandDiv.length == 1) {
+				System.out.println("명령어 확인해");
+				continue;
+			}
+
+			String actionMethodName = commandDiv[1];
+
+			controller = null;
+
+			if (controllerName.equals("article")) {
+				controller = articleController;
+			} else if (controllerName.equals("member")) {
+				controller = memberController;
+			} else {
+				System.out.println("존재하지 않는 기능이야");
+				continue;
+			}
+
+			controller.doAction(actionMethodName, command);
+
+		}
+
+		System.out.println("== 프로그램 종료 ==");
+
+		sc.close();
+	}
+}
---
+++ src/com/KoreaIT/java/BAM/controller/MemberController.java
@@ -0,0 +1,131 @@
+package com.KoreaIT.java.BAM.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.KoreaIT.java.BAM.dto.Member;
+import com.KoreaIT.java.BAM.util.Util;
+
+public class MemberController extends Controller {
+
+	private List<Member> members;
+	private Scanner sc;
+	private String actionMethodName;
+	private String command;
+
+	int lastMemberId = 3;
+
+	public MemberController(Scanner sc) {
+		this.members = new ArrayList<Member>();
+		this.sc = sc;
+	}
+
+	public void doAction(String actionMethodName, String command) {
+		this.actionMethodName = actionMethodName;
+		this.command = command;
+
+		switch (actionMethodName) {
+		case "join":
+			doJoin();
+			break;
+		default:
+			System.out.println("그런 세부기능은 없어");
+			break;
+		}
+	}
+
+	public void doJoin() {
+		int id = lastMemberId + 1;
+		String regDate = Util.getNow();
+		String loginId = null;
+		String loginPw = null;
+		String loginPwConfirm = null;
+		String name = null;
+
+		while (true) {
+			System.out.printf("로그인 아이디 : ");
+			loginId = sc.nextLine();
+
+			if (loginId.length() == 0) {
+				System.out.println("아이디 입력해라");
+				continue;
+			} else if (isJoinableLoginId(loginId) == false) {
+				System.out.println("이미 쓰는 아이디야");
+				continue;
+			}
+
+			break;
+		}
+
+		while (true) {
+			System.out.printf("로그인 비밀번호 : ");
+			loginPw = sc.nextLine();
+			if (loginPw.length() == 0) {
+				System.out.println("비밀번호 입력해라");
+				continue;
+			}
+			while (true) {
+				System.out.printf("로그인 비밀번호 확인 : ");
+				loginPwConfirm = sc.nextLine();
+
+				if (loginPwConfirm.length() == 0) {
+					System.out.println("비밀번호 확인 입력해라");
+					continue;
+				}
+				break;
+			}
+
+			if (loginPw.equals(loginPwConfirm) == false) {
+				System.out.println("비밀번호 확인해");
+				continue;
+			}
+			break;
+		}
+
+		while (true) {
+			System.out.printf("이름 : ");
+			name = sc.nextLine();
+
+			if (name.length() == 0) {
+				System.out.println("이름 입력해라");
+				continue;
+			}
+			break;
+		}
+
+		Member member = new Member(id, regDate, regDate, loginId, loginPw, name);
+		members.add(member);
+
+		System.out.printf("%d번 회원이 가입되었습니다.\n", id);
+		lastMemberId++;
+		
+	}
+
+	private boolean isJoinableLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public void makeTestData_member() {
+		System.out.println("테스트를 위한 회원데이터 3개 생성 완료");
+		members.add(new Member(1,Util.getNow(),Util.getNow(),"123","123","이름 : 홍길동"));
+		members.add(new Member(2,Util.getNow(),Util.getNow(),"abc","abc","이름 : 홍길순"));
+		members.add(new Member(3,Util.getNow(),Util.getNow(),"bbc","bbc","이름 : 홍길윤"));
+	}
+
+}
